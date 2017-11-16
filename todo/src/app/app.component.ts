938998c89ef548c3ba78cae04dba68d2e89d98be
sajinovics <srdjan.sajinovic@lanaco.com>
2017-11-16 14:15:27 +0100

    omogućen ispis imena korisnika

--- todo/src/app/app.component.ts
+++ todo/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { Model, TodoItem } from "./model";
+
 
 @Component({
-  selector: 'app-root',
+  selector: "todo-app",
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  model = new Model();
+  getName() {
+    return this.model.user;
+  }
 }
