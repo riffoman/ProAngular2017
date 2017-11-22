efa773972eaf3fe534881827a725feff09c8779e
sajinovics <srdjan.sajinovic@lanaco.com>
2017-11-22 09:04:43 +0100

    umjesto kastovanja sa < i > može se koristiti i keyword as... a umjesto deklarisanja tipa number | string, može se deklarisati tip any. ako ne deklarišemo nikakav tip, onda ts po defaultu koristi any

--- JavaScriptPrimer/tempConverter.js
+++ JavaScriptPrimer/tempConverter.js
@@ -7,6 +7,19 @@ var TempConverter = (function () {
             ? temp : parseFloat(temp);
         return ((parseFloat(value.toPrecision(2)) - 32) / 1.8).toFixed(1);
     };
+    TempConverter.convertFtoC_version2 = function (temp) {
+        var value;
+        if (temp.toPrecision) {
+            value = temp;
+        }
+        else if (temp.indexOf) {
+            value = parseFloat(temp);
+        }
+        else {
+            value = 0;
+        }
+        return ((parseFloat(value.toPrecision(2)) - 32) / 1.8).toFixed(1);
+    };
     return TempConverter;
 }());
 exports.TempConverter = TempConverter;
--- JavaScriptPrimer/tempConverter.ts
+++ JavaScriptPrimer/tempConverter.ts
@@ -4,4 +4,16 @@ export class TempConverter {
             ? <number>temp : parseFloat(<string>temp);
         return ((parseFloat(value.toPrecision(2)) - 32) / 1.8).toFixed(1);
     }
+
+    static convertFtoC_version2(temp: any): string {
+        let value: number;
+        if ((temp as number).toPrecision) {
+            value = temp;
+        } else if ((temp as string).indexOf) {
+            value = parseFloat(<string>temp);
+        } else {
+            value = 0;
+        }
+        return ((parseFloat(value.toPrecision(2)) - 32) / 1.8).toFixed(1);
+    }
 }
