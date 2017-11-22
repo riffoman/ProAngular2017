2be99c5e742ad8361a83fdbbaff2b7366e54c402
sajinovics <srdjan.sajinovic@lanaco.com>
2017-11-22 08:46:42 +0100

    ne postoji polimorfizam metoda u JS i TS pa se zato metode koje mogu primiti number i string pišu na ovaj način, mora se provjeriti koji tip je primljen u metodi i za svaki odraditi logiku

--- JavaScriptPrimer/tempConverter.js
+++ JavaScriptPrimer/tempConverter.js
@@ -3,7 +3,9 @@ var TempConverter = (function () {
     function TempConverter() {
     }
     TempConverter.convertFtoC = function (temp) {
-        return ((parseFloat(temp.toPrecision(2)) - 32) / 1.8).toFixed(1);
+        var value = temp.toPrecision
+            ? temp : parseFloat(temp);
+        return ((parseFloat(value.toPrecision(2)) - 32) / 1.8).toFixed(1);
     };
     return TempConverter;
 }());
