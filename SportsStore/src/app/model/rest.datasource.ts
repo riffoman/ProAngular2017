cb6fee81de7216b573f54d39d36e3ea8a5b93dc5
sajinovics <srdjan.sajinovic@lanaco.com>
2017-11-28 15:48:36 +0100

    uvedena autentikacija sa tokenom, može se autentikovati samo ako imamo admin/secret kredencijale.

--- SportsStore/src/app/model/rest.datasource.ts
+++ SportsStore/src/app/model/rest.datasource.ts
@@ -10,21 +10,42 @@ const PORT = 3500;
 @Injectable()
 export class RestDataSource {
     baseUrl: string;
+    auth_token: string;
     constructor(private http: Http) {
         this.baseUrl = `${PROTOCOL}://${location.hostname}:${PORT}/`;
     }
+
+    authenticate(user: string, pass: string): Observable<boolean> {
+        return this.http.request(new Request({
+            method: RequestMethod.Post,
+            url: this.baseUrl + "login",
+            body: { name: user, password: pass }
+        })).map(response => {
+            let r = response.json();
+            this.auth_token = r.success ? r.token : null;
+            return r.success;
+        });
+    }
+
     getProducts(): Observable<Product[]> {
-        return this.sendRequest(RequestMethod.Get, "products");
+        return this.sendRequest(RequestMethod.Get, "products") as Observable<Product[]>;
     }
     saveOrder(order: Order): Observable<Order> {
-        return this.sendRequest(RequestMethod.Post, "orders", order);
+        return this.sendRequest(RequestMethod.Post,
+            "orders", order) as Observable<Order>;
     }
+
     private sendRequest(verb: RequestMethod,
-        url: string, body?: Product | Order): Observable<Product | Order> {
-        return this.http.request(new Request({
+        url: string, body?: Product | Order, auth: boolean = false)
+        : Observable<Product | Product[] | Order | Order[]> {
+        let request = new Request({
             method: verb,
             url: this.baseUrl + url,
             body: body
-        })).map(response => response.json());
+        });
+        if (auth && this.auth_token != null) {
+            request.headers.set("Authorization", `Bearer<${this.auth_token}>`);
+        }
+        return this.http.request(request).map(response => response.json());
     }
 }
